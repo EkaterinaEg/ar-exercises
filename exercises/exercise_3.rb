29f902bade01aaca11ecbbd6981b2c07ba6bba17
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-12-12 04:05:47 -0500

    implement exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,17 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
+# bundle exec ruby exercises/exercise_3.rb
 # Your code goes here ...
+### Exercise 3: Delete the third store
+
+# 1. Load the third store (into `@store3`) as you did the other two before.
+
+@store3 = Store.find_by(id: 3)
+puts @store3.inspect
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+
+@store3 .destroy
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+
+puts Store.count
